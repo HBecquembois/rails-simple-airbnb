ea9f0f0ba5d13bf802470b491444af73fb1127cc
Helene Becquembois <hbecquembois@gmail.com>
2023-06-26 12:36:53 +0200

    flats index and flat show OK

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,3 +1,20 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit]
   
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
